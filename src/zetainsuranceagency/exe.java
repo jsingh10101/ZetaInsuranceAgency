be7cd13cfaa88bcba93499b66d914feaf28e3ce0
JAPNEET SINGH <japneet.theultimateenlightment@gmail.com>
2018-12-17 21:15:53 +0530

    12th project

---
+++ src/zetainsuranceagency/exe.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package zetainsuranceagency;
+
+/**
+ *
+ * @author thegr_000
+ */
+public class exe {
+    public static String pno;
+    public static String agn;
+     public static String back;
+}
